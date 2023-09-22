200a6bcd774e9f68b50ddcb9ed33e7b496955f83
karlocirkvencic <cirkvencic.karlo@gmail.com>
2023-09-22 06:59:27 +0200

    Workspace podređenog računala
    DIrektorij korišten pri izradi završnog rada

---
+++ robotino_ws_zavrsni_rad/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/wcr-pc/robotino_ws/src
+# Build directory: /home/wcr-pc/robotino_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robotino")
